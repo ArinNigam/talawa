d45a78c56d91691e2c8b2492271ea12e3d4eab16
Arin Nigam <arin.nigam@gmail.com>
2024-03-30 10:32:34 +0530

    Revert "Add chat message documentation"
    This reverts commit 2a52272cde4751013ee3197c85d574d159938b92.

--- lib/views/after_auth_screens/chat/widgets/chat_message_bubble.dart
+++ lib/views/after_auth_screens/chat/widgets/chat_message_bubble.dart
@@ -10,7 +10,6 @@ import 'package:talawa/services/size_config.dart';
 class Message extends StatelessWidget {
   const Message({super.key, required this.message});
 
-  /// The chat message that this widget represents.
   final ChatMessage message;
 
   @override
